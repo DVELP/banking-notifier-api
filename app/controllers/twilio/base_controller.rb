22854b438b3736d6d7a28c2ae51d90dd1f3471e0
Vitaliy Bezkrovny <vitaly.beskrovny@gmail.com>
2019-10-02 16:36:16 +0300

    + Twillio::Voice::BaseController is base controller for all Twillio controlers

---
+++ app/controllers/twilio/base_controller.rb
@@ -0,0 +1,24 @@
+module Twilio
+  class BaseController < ApplicationController
+    before_action :validate_request
+
+
+    private 
+
+	def validate_request
+	  request_verification_service =
+	    RequestVerificationService.new(
+	      ::Twilio::Security::RequestValidator.new(
+	        ENV.fetch('TWILIO_AUTH_TOKEN')
+	      )
+	    )
+
+	  unless request_verification_service.verify(
+	    uri: request.original_url,
+	    env: request.env
+	  )
+	    render head :ok, status: :unauthorized && return
+	  end
+	end
+  end
+end
