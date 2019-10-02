a01f50b78edbd1cd285c5492b4866e98c2985796
Vitaliy Bezkrovny <vitaly.beskrovny@gmail.com>
2019-10-02 16:34:01 +0300

    + routess for interactions and incomings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
+  namespace :twilio do
+    namespace :voice do
+      resources :interactions, only: [:create]
+      resources :incomings, only: [:create]
+    end
+  end
 end
