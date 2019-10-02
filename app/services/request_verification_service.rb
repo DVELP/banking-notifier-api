ebfa2c9895633d397b1d33ec1c7c237b9797d929
Vitaliy Bezkrovny <vitaly.beskrovny@gmail.com>
2019-10-02 16:34:59 +0300

    + RequestVerificationService service for authorization Twilio requests

---
+++ app/services/request_verification_service.rb
@@ -0,0 +1,17 @@
+class RequestVerificationService
+  attr_accessor :validator
+
+  def initialize(validator)
+    self.validator = validator
+  end
+
+  def verify(uri:, env:)
+    params = if env['REQUEST_METHOD'] == 'POST'
+              env['rack.request.form_hash']
+            else
+              env['rack.request.query_hash']
+            end
+    signature = env['HTTP_X_TWILIO_SIGNATURE']
+    signature.present? && validator.validate(uri, params, signature)
+  end
+end
